d2a046d4bd8e9ebfe60d54c05477a21db115fd0a
Khaled <khaledesam384@gmail.com>
2024-05-29 16:36:33 +0300

    Resolvers are relocated into graphql directory

--- src/graphql/schemas/AttributeSetTypeDefinition.php
+++ src/graphql/schemas/AttributeSetTypeDefinition.php
@@ -17,12 +17,12 @@ class AttributeSetType extends BaseType
                 'resolve' => function ($parent, $args, $context) {
                     $items = [];
                     foreach ($parent['items'] as $item) {
-                    // Construct each item object
-                    $itemObject = [
-                    'id' => $item['id'],
-                    'displayValue' => $item['displayValue'],
-                    'value' => $item['value']
-                    ];
+                        // Construct each item object
+                        $itemObject = [
+                        'id' => $item['id'],
+                        'displayValue' => $item['displayValue'],
+                        'value' => $item['value']
+                        ];
                     
                     // Append the item object to the items array
                     $items[] = $itemObject;
--- src/graphql/schemas/ProductTypeDefinition.php
+++ src/graphql/schemas/ProductTypeDefinition.php
@@ -10,64 +10,64 @@ class ProductType extends BaseType
 
     private $queryResolvers;
 
-public function __construct($queryResolvers)
-{
-$this->queryResolvers = $queryResolvers;
-$name = 'Product';
-$fields = [
-'id' => Type::string(),
-'name' => Type::string(),
-'inStock' => Type::boolean(),
-'gallery' => [
-'type' => Type::listOf(Type::string()), // Assuming gallery is a list of strings (image URLs)
-'resolve' => function ($product, $args ) {
-return $this->queryResolvers['Gallery']->resolve($product['id']);
-},
-],
-'description' => Type::string(),
-'category' => [
-'type' => Type::string(),
-'resolve' => function ($product, $args ) {
-return $this->queryResolvers['CategoryName']->resolve($product['category_id']);
-},
-],
-'brand' => Type::string(),
-'prices' => [
-'type' => (new PriceType())->getType(),
-'resolve' => function ($product, $args ) {
-return $this->queryResolvers['Price']->resolve($product['price_id']);
-},
-],
-'attributes' => [
-'type' => Type::listOf((new AttributeSetType())->getType()),
-'resolve' => function ($product, $args ) {
-$attributesData = $this->queryResolvers['Attribute']->resolve($product['id']);
-$attributes = [];
+    public function __construct($queryResolvers)
+    {
+    $this->queryResolvers = $queryResolvers;
+    $name = 'Product';
+    $fields = [
+    'id' => Type::string(),
+    'name' => Type::string(),
+    'inStock' => Type::boolean(),
+    'gallery' => [
+        'type' => Type::listOf(Type::string()), // Assuming gallery is a list of strings (image URLs)
+        'resolve' => function ($product, $args ) {
+            return $this->queryResolvers['Gallery']->resolve($product['id']);
+        },
+    ],
+    'description' => Type::string(),
+    'category' => [
+        'type' => Type::string(),
+        'resolve' => function ($product, $args ) {
+            return $this->queryResolvers['CategoryName']->resolve($product['category_id']);
+        },
+    ],
+    'brand' => Type::string(),
+    'prices' => [
+        'type' => (new PriceType())->getType(),
+        'resolve' => function ($product, $args ) {
+            return $this->queryResolvers['Price']->resolve($product['price_id']);
+        },
+    ],
+    'attributes' => [
+        'type' => Type::listOf((new AttributeSetType())->getType()),
+        'resolve' => function ($product, $args ) {
+            $attributesData = $this->queryResolvers['Attribute']->resolve($product['id']);
+            $attributes = [];
 
-// Group attributes by ID
-$groupedAttributes = [];
-foreach ($attributesData['id'] as $key => $id) {
-$groupedAttributes[$id][] = [
-'displayValue' => $attributesData['displayValue'][$key],
-'value' => $attributesData['value'][$key],
-'id' => $attributesData['displayValue'][$key], // Assuming 'id' is similar to 'displayValue'
-];
-}
+            // Group attributes by ID
+            $groupedAttributes = [];
+            foreach ($attributesData['id'] as $key => $id) {
+                $groupedAttributes[$id][] = [
+                'displayValue' => $attributesData['displayValue'][$key],
+                'value' => $attributesData['value'][$key],
+                'id' => $attributesData['displayValue'][$key],
+                ];
+            }
 
-// Construct AttributeSet for each group
-foreach ($groupedAttributes as $id => $items) {
-$attributeSet = [
-'id' => $id,
-'items' => $items,
-];
-$attributes[] = $attributeSet;
-}
+            // Construct AttributeSet for each group
+            foreach ($groupedAttributes as $id => $items) {
+                $attributeSet = [
+                'id' => $id,
+                'items' => $items,
+                ];
+                $attributes[] = $attributeSet;
+            }
 
-return $attributes;
-},
-],
-];
+            return $attributes;
+        },
+    ],
+    ];
 
-parent::__construct($name, $fields);
-}
+    parent::__construct($name, $fields);
+    }
 }
